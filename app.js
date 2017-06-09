57417ec847b6a95e22acc57938e8b4a50dfb50ff
Laura Wallace <lauranestwallace@gmail.com>
2017-06-09 12:28:31 -0400

    added comments to note features that to be addressed

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ $("form").on("submit", function(event) {
 })
 
 function search(query) {
-
 	var url = "http://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b"
+//Need to add a way to check whether the city entered is a valid city name and 
+//if it is not then should alert user to try again with a valid city name
+
 	$(".js-weather").html('loading weather...')
+
 	$.getJSON(url, function(response) {
-		$(".js-weather").html('')
+		$(".js-weather").html('') 
 		var currentTemp = response.main.temp;
 		var currentForecast = response.weather[0].main;
 		var cityName = response.name;
@@ -38,3 +41,8 @@ function search(query) {
 })
 }
 
+//Need to implement change to api to test out the 10 day forecast data
+//New URL needs to be added
+//Variables need to be changed to match new URL responses for extended forecast data
+//Decide How to handle 10 day data (i.e. add a new button and new table
+//How to handle unix date code conversion for 10 day forecast
