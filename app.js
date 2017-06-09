02d1ad340fe9e47db13fe6f0b42107a6b7b98a94
Laura Wallace <lauranestwallace@gmail.com>
2017-06-09 10:27:37 -0400

    changed weather data to display in table format instead of p format

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function search(query) {
 	$(".js-weather").html('loading weather...')
 	$.getJSON(url, function(response) {
 		$(".js-weather").html('')
-		var weatherString = "";
+//		var weatherString = "";
 		var currentTemp = response.main.temp;
 		var currentForecast = response.weather[0].description;
 		var cityName = response.name;
@@ -20,16 +20,32 @@ function search(query) {
 		var currentWind = response.wind.speed;
 		//used parantheses to show string is multi-line
 		var testString = (`
-			<p> The current temperature in ${cityName} is ${currentTemp} degrees Fahrenheit. </p>
-			<p> The current weather forecast is: ${currentForecast}. </p>
-			<p> The relative humity is ${currentHumidity}% and the wind speed is ${currentWind} miles per hour. </p>
+//			<p> The current temperature in ${cityName} is ${currentTemp} degrees Fahrenheit. </p>
+//			<p> The current weather forecast is: ${currentForecast}. </p>
+//			<p> The relative humity is ${currentHumidity}% and the wind speed is ${currentWind} miles per hour. </p>
   			`);
-
+		var weatherTable = (`
+			<table> 
+				<tr>
+    				<th>City Name</th>
+    				<th>Current Temperature (F)</th>
+   		 			<th>Current Weather Forecast</th>
+   		 			<th>Relative Humidity (%)</th>
+   		 			<th>Wind Speed (mph)</th>
+  				</tr>
+  				<tr>
+    				<td>${cityName}</td>
+    				<td>${currentTemp}</td>
+    				<td>${currentForecast}</td>
+    				<td>${currentHumidity}</td>
+    				<td>${currentWind}</td>
+  				</tr>
+  				`)
 //used concatenation to make the weather string easier to see
 //	weatherString+= "<h3>" + "The current temperature in " + cityName + " is " + currentTemp + " degrees Fahrenheit." + "</h3>"
 //	weatherString+= "<h3>" + "The current weather is " + currentForecast + " ." + "</h3>"
 //	weatherString+= "<h3>" + "The relative humidity is " + currentHumidity + "% and the wind speed is " + currentWind + " miles per hour." + "</h3>";
-	$(".js-weather").append(testString)
+	$(".js-weather").append(weatherTable)
 })
 }
 
