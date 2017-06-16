4720a4dc7b63ec08b9773727313561acea59fc7c
Laura Wallace <lauranestwallace@gmail.com>
2017-06-16 10:54:22 -0400

    added output table for forecast

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $("form").on("submit", function(event) {
 		// add in separate search for other query here
 		searchForecast(query);
 			function searchForecast(query) {
-				var forecastUrl = "api.openweathermap.org/data/2.5/forecast/daily?q=" + query + "&units=imperial&cnt=10&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b";
+				var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + query + "&units=imperial&cnt=10&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b";
 				var forecastOutput = 'loading...';
 				$(".js-weather").html(forecastOutput)
 				$.getJSON(forecastUrl, function(response) {
@@ -27,6 +27,7 @@ $("form").on("submit", function(event) {
 					var tempFive = response.list[4].temp.day;
 					var tempSix = response.list[5].temp.day;
 					var tempSeven = response.list[6].temp.day;
+					console.log(tempOne)
 					var forecastOne = response.list[0].weather.description;
 					var forecastTwo = response.list[1].weather.description;
 					var forecastThree = response.list[2].weather.description;
@@ -34,6 +35,50 @@ $("form").on("submit", function(event) {
 					var forecastFive = response.list[4].weather.description;
 					var forecastSix = response.list[5].weather.description;
 					var forecastSeven = response.list[6].weather.description;
+
+					forecastOutput = (`
+						<table> 
+							<tr>
+    							<th>Date</th>
+    							<th>Day Temperature (F)</th>
+   		 						<th>Weather Forecast</th>
+  							</tr>
+  							<tr>
+    							<td>${dateOne}</td>
+    							<td>${tempOne}</td>
+    							<td>${forecastOne}</td>
+  							</tr>
+  							<tr>
+    							<td>${dateTwo}</td>
+    							<td>${tempTwo}</td>
+    							<td>${forecastTwo}</td>
+  							</tr>
+  							<tr>
+    							<td>${dateThree}</td>
+    							<td>${tempThree}</td>
+    							<td>${forecastThree}</td>
+  							</tr>
+  							<tr>
+    							<td>${dateFour}</td>
+    							<td>${tempFour}</td>
+    							<td>${forecastFour}</td>
+  							</tr>
+  							<tr>
+    							<td>${dateFive}</td>
+    							<td>${tempFive}</td>
+    							<td>${forecastFive}</td>
+  							</tr>
+  							<tr>
+    							<td>${dateSix}</td>
+    							<td>${tempSix}</td>
+    							<td>${forecastSix}</td>
+  							</tr>
+  							<tr>
+    							<td>${dateSeven}</td>
+    							<td>${tempSeven}</td>
+    							<td>${forecastSeven}</td>
+  							</tr>
+  				`)
 				})
 			}
 	}
@@ -41,7 +86,7 @@ $("form").on("submit", function(event) {
 })
 
 function search(query) {
-	var url = "//api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b";
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b";
 	var output = 'loading...';
 	$(".js-weather").html(output)
 
