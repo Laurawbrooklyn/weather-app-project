49f209e89297c5494ca59be32b4b5d7cc4e7d7ea
Laura Wallace <lauranestwallace@gmail.com>
2017-07-19 14:39:16 -0400

    all changes only to deploy app to heroku to fix cors issue

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+app.use(express.static('public'));	
+app.use(cors());
+const expressNunjucks = require('express-nunjucks');
+
+const isDev = app.get('env') === 'development';
+ 
+app.set('views', __dirname + '/templates');
+ 
+const njk = expressNunjucks(app, {
+    watch: isDev,
+    noCache: isDev
+});
+
+app.get("/", (req, res) => {
+	const message = 'hola mundo';
+	res.render('index');
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
