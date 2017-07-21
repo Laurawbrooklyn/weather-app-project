2c86c95e2b8cb3d04c21dd9036ab6d764fc2ebb7
Laura Wallace <lauranestwallace@gmail.com>
2017-07-21 14:00:04 -0400

    fixed cors issue

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const express = require('express');
 const app = express();
 //var cors = require('cors');
@@ -24,7 +25,23 @@ app.get("/", (req, res) => {
 	const message = 'hola mundo';
 	res.render('index');
 });
+app.get('/data/*', (req, res) => {
+	request.get("http://api.openweathermap.org" + req.url, {
+ //   gzip: true,
+      json: true
+  	}, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+//      resolve(body);
+		res.json(body);
+      } else {
+//      reject(error);
+		res.error(error);
+      }
+    });
 
+})
+//http://api.openweathermap.org/data/2.5/forecast/daily?q=new%20york&units=imperial&cnt=7&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b
+//http://api.openweathermap.org/data/2.5/weather?q=new%20york&units=imperial&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
