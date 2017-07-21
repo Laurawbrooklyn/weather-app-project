01d959c3d1b64e2f7798cc6dad401ef8bf29c9ac
Laura Wallace <lauranestwallace@gmail.com>
2017-07-21 13:23:13 -0400

    testing out new solution to cors issue

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
-app.use(cors({credentials: true, origin: true}));
-app.options('*', cors());
+//var cors = require('cors');
+//app.use(cors({credentials: true, origin: true}));
+//app.options('*', cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.static('public'));
 const expressNunjucks = require('express-nunjucks');
 
