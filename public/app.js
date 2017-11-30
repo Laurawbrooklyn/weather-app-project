b4b0ae125eea6698469191c860f98f1d045bca19
Laura Wallace <lauranestwallace@gmail.com>
2017-11-30 11:57:01 -0500

    fixed pictures and app is working

--- public/app.js
+++ public/app.js
@@ -5,34 +5,37 @@ $(".search-button").on("click", function(event) {
 })
 
 
-	function search(query) {
-		var url = "/data/2.5/weather?q=" + query + "&units=imperial&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b";
-		var output = 'loading...';
-		$(".js-weather").html(output)
+function search(query) {
+	var url = "/data/2.5/weather?q=" + query + "&units=imperial&appid=bcfbfcaf752dc3dc0f4547e42bd0d35b";
+	var output = 'loading...';
+	$(".js-weather").html(output)
 
-		$.getJSON(url, function(response) {
+	$.getJSON(url, function(response) {
 
-			console.log(response)
-			var currentTemp = Math.round(response.main.temp);
-			var currentForecast = response.weather[0].main;
-			var cityName = response.name;
-			var currentHumidity = response.main.humidity;
+		console.log(response)
+		var currentTemp = Math.round(response.main.temp);
+		var currentForecast = response.weather[0].main;
+		var cityName = response.name;
+		var currentHumidity = response.main.humidity;
 
-			$(".rain-page").removeClass("hidden")
-			$(".index-page").addClass("hidden")
-			$("#location").append(cityName)
-			$("#current-temp").append(currentTemp)
-			$("#current-forecast").append(currentForecast)
-			$("#current-humidity").append(currentHumidity)
+		$(".rain-page").removeClass("hidden")
+		$(".index-page").addClass("hidden")
+		$("#location").append(cityName)
+		$("#current-temp").append(currentTemp)
+		$("#current-forecast").append(currentForecast)
+		$("#current-humidity").append(currentHumidity)
 
-			if (response.weather[0].main==="Clear"){
-				$("#top-text").text('Go Outside')
-				$('.rain-image').css("background-image", "url(/clear.png)");
+		if (response.weather[0].main==="Clear"){
+			$("#top-text").text('Go Outside')
+			$('.rain-image').css("background-image", "url(/clear.png)");
 
-			} else if (response.weather[0].main==="Rain"){
-				$("#top-text").text('Stay Dry!')
-			}
-
+		} else if (response.weather[0].main==="Rain"){
+			$("#top-text").text('Stay Dry!')
+			$('.rain-image').css("background-image", "url(/rain.jpg)");
+		} else if (response.weather[0].main==="Snow"){
+			$("#top-text").text('It is cold outside!')
+			$('.rain-image').css("background-image", "url(/snowman.jpg)");
+		}
 
 			}).fail(function() {
 				//output = "Sorry we couldn't find that city, please try again!";
